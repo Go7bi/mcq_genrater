69cdad8013e8e9ad2d306ec0b871e2e2d232dd5f
Go7bi <gobimurugan71@gmail.com>
2025-05-28 13:46:21 +0530

    got it

--- src/mcqgenrater/__init__.py
+++ src/mcqgenrater/__init__.py
@@ -4,7 +4,7 @@ setup(
     name='mcqgenrator',
     version='0.0.1',
     author='gobi',
-    author_email='sunny.savita@ineuron.ai',
+    author_email='gobi.gobinuro.ai',
     install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
